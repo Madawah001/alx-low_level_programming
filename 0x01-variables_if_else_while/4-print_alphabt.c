ba7160f3383f46ffd68f524b15642eec0cc8641d
Madawah001 <lucahmadawah@gmail.com>
2023-06-17 11:05:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lowercase
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'd'; c++)
+	putchar(c);
+	for (c = 'f'; c <= 'p'; c++)
+	putchar(c);
+	for (c = 'r'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+
+}
