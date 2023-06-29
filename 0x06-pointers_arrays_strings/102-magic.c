d43b54e523630e356a60b1c514918809f0269794
Madawah001 <lucahmadawah@gmail.com>
2023-06-29 13:37:14 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,8 +11,8 @@ p = &n;
 /*
 * write your line of code here...
 * Remember:
-* - you are not allowed to use a in your new line of code
-* - you are not allowed to modify the variable  p
+* - you are not allowed to use a
+* - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
